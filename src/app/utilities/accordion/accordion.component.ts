00912619847211e584da950bc052ed289ef854c6
Sai <saikumar@divami.com>
2019-04-17 19:53:29 +0530

    Add side-nav accordion interaction

--- src/app/utilities/accordion/accordion.component.ts
+++ src/app/utilities/accordion/accordion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormDataService } from 'src/app/services/form-data.service';
 
 @Component({
     selector: 'app-accordion',
@@ -7,18 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccordionComponent implements OnInit {
     openAccordionIndex;
-    accordionData = [1, 2]
+    accordionData = [1, 2];
+    questionnaireData: any;
+    currentPage: any;
 
-    constructor() { }
+    constructor(private formData: FormDataService) { }
 
     ngOnInit() {
+        let questionnaireData = this.formData.getFormData();
+        if(questionnaireData && questionnaireData.data && questionnaireData.currentPage) {
+            this.questionnaireData = questionnaireData.data.data;
+            this.currentPage = questionnaireData.currentPage;
+        }
+
     }
+    
     onTabOpen(event) {
         this.openAccordionIndex = event.index;
     }
     onTabClose(event) {
         this.openAccordionIndex = undefined;
     }
+
 
 
 
