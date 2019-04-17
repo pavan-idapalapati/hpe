379d573a729d8c07558613ac1ac1b5901c25d290
pavan <pavankumar@divami.com>
2019-04-17 17:26:11 +0530

    Add side nav transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,29 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormDataService } from './services/form-data.service';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  
-  constructor(private router: Router, private formData: FormDataService) {}
+    openSideNavFlag;
+    @ViewChild('rightNavAccordion') rightNavAccordion: any;
 
-  ngOnInit() {
-		this.init();
-	}
-  
-  init() {
-    var formData = this.formData.getFormData();
-    if(!formData || !formData.currentPage || !formData.data) {
-      this.router.navigate(['/']);
-    } else if(formData && formData.currentPage && formData.data && formData.currentPage < formData.data.data.length) {
-      this.router.navigate(['/questionaire']);
-    } else {
-      this.router.navigate(['/conclusion']);
+    constructor(private router: Router, private formData: FormDataService) { }
+
+    ngOnInit() {
+        this.init();
     }
-  }
+
+    init() {
+        var formData = this.formData.getFormData();
+        if (!formData || !formData.currentPage || !formData.data) {
+            this.router.navigate(['/']);
+        } else if (formData && formData.currentPage && formData.data && formData.currentPage < formData.data.data.length) {
+            this.router.navigate(['/questionaire']);
+        } else {
+            this.router.navigate(['/conclusion']);
+        }
+    }
+
+    openSideNav() {
+        this.openSideNavFlag = true;
+        this.rightNavAccordion.nativeElement.classList
+        .remove('remove-right-nav-accordion-transition')
+        this.rightNavAccordion.nativeElement.classList
+        .add('right-nav-accordion-transition');
+
+    }
+    closeSideNav() {
+        this.openSideNavFlag = false;
+        this.rightNavAccordion.nativeElement.classList.remove('right-nav-accordion-transition')
+        this.rightNavAccordion.nativeElement.classList.add('remove-right-nav-accordion-transition');
+
+    }
 
 }
