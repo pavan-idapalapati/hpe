d2a02982df4536e1fedcafbe08987f4085941f55
pavan <pavankumar@divami.com>
2019-04-17 12:33:08 +0530

    Integrated ng prime interfaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AccordionModule} from 'primeng/accordion';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AccordionModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
