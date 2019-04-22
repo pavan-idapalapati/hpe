8848e8246db58b834a10c91a3e79ce1ebe876208
pavan <pavankumar@divami.com>
2019-04-22 18:49:55 +0530

    header issue fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener, } from '@angular/core';
 import { FormDataService } from './services/form-data.service';
 
 import { Router, NavigationStart, NavigationExtras } from '@angular/router';
@@ -11,16 +11,16 @@ import { UtilService } from './services/util.service';
 })
 export class AppComponent implements OnInit {
 
-    isLandingPage:boolean = true;
+    isLandingPage: boolean = true;
 
     constructor(private router: Router, private formData: FormDataService, private utils: UtilService) {
         router.events.subscribe(event => {
-            if(event instanceof NavigationStart) {
-                let url = event.url.substring(0, (event.url.indexOf("?")> -1 ? event.url.indexOf("?"): event.url.length-1) ).trim();
-               if(url == '/' || url == ''){
+            if (event instanceof NavigationStart) {
+                let url = event.url.substring(0, (event.url.indexOf("?") > -1 ? event.url.indexOf("?") : event.url.length - 1)).trim();
+                if (url == '/' || url == '') {
                     this.isLandingPage = false;
                 }
-                else{
+                else {
                     this.isLandingPage = true;
                 }
             }
@@ -33,7 +33,7 @@ export class AppComponent implements OnInit {
 
     init() {
         let hasSession = this.utils.getSessionStatusFromCookies();
-        if(hasSession) {
+        if (hasSession) {
             var formData = this.formData.getFormData();
             if (!formData || !formData.currentPage || !formData.data) {
                 let navigationExtras: NavigationExtras = {
@@ -45,7 +45,7 @@ export class AppComponent implements OnInit {
             } else {
                 this.router.navigate(['/']);
             }
-        } else  {
+        } else {
             let navigationExtras: NavigationExtras = {
                 queryParams: {
                     "new": true
@@ -54,5 +54,16 @@ export class AppComponent implements OnInit {
             this.router.navigate(['/'], navigationExtras);
         }
     }
+
+
+    @HostListener('scroll', ['$event']) private onScroll($event: Event): void {
+        let top;
+        if($event.srcElement.scrollTop <= 100) {
+             top = 100-$event.srcElement.scrollTop
+        } else if ($event.srcElement.scrollTop > 100) {
+            top = 0;
+        }
+        document.getElementById('left-side-section').style.top = `${top}px`;
+    };
 
 }
