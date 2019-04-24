e0d1a83c5e10f6b5c004a7b8105ed1ae0d14be63
Mr_Green <kamlekar.venkatesh@gmail.com>
2019-04-25 03:01:34 +0530

    asset pipe fix for demo

--- src/app/pipe/assets.pipe.ts
+++ src/app/pipe/assets.pipe.ts
@@ -8,7 +8,10 @@ export class AssetsPipe implements PipeTransform {
     transform(value: string, args?: any): any {
         if (document.location.origin.indexOf('localhost') > -1) {
             return './' + value;
-        } else if (document.location.pathname.indexOf('service.labs') === -1) {
+        } else if (document.location.pathname.indexOf('hpe') > -1) {
+            return 'http://demo.divami.com/hpe/' + value;
+        } 
+        else if (document.location.pathname.indexOf('service.labs') === -1) {
             return 'https://edge.spiceworksstatic.com/service.labs/' + document.location.pathname.split('/')[1] + '/' + value;
         } else {
             return './' + value;
